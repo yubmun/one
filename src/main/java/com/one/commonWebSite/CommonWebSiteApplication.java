66418ff5f9ad3eec31085894a14694eba0298c9e
yubmun <dlwnsduq23@gmail.com>
2022-07-27 20:03:21 +0900

    1

---
+++ src/main/java/com/one/commonWebSite/CommonWebSiteApplication.java
@@ -0,0 +1,13 @@
+package com.one.commonWebSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonWebSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonWebSiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/one/commonWebSite/CommonWebSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.one.commonWebSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonWebSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
